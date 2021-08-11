ecbbf5119eed636094876a054e7a00df1c33368a
gacasti <gilmarmcm@gmail.com>
2021-08-11 17:41:15 -0600

    initiated editing bookings

--- routes/booking.js
+++ routes/booking.js
@@ -98,33 +98,40 @@ router.post("/book", function (req, res, next) {
 router.get("/bookings/:userid", function (req, res, next) {
   const userid = req.params.userid;
   console.log(`******* This is the user id from Bookings: ${userid} *********`);
-  Booking.find({ CustomerId: userid }, (err, userGBokings) => {
-    if (err) console.log(err);
-    console.log(userGBokings);
-    res.render("bookings", { bookings: userGBokings });
-  });
+  Booking.find({ CustomerId: userid })
+    .populate("PackageId")
+    .exec((err, userGBokings) => {
+      if (err) console.log(err);
+      console.log(userGBokings);
+      res.render("bookings", { bookings: userGBokings });
+    });
 });
 
-/* GET the purchases page. */
-router.get("/purchases/", function (req, res, next) {
-  Purchase.find({ userId: 3 })
-    // Replace the productId with the corresponding product object from the products collection(table)
-    .populate("productId")
-    .exec((err, purchases) => {
-      if (err) console.log(err);
-      res.render("purchases", { purchases });
+// Process the edited product data
+router.post("/edit/:bookingid", function (req, res, next) {
+  const bookingId = req.params.bookingid;
+  new Bookingdetail(req.body).validate((err) => {
+    // To validate the data before updating
+    if (err)
+      return processErrors(err, "bookingadd", req, res, {
+        add: false,
+        prod: { ...req.body, _id: bookingId },
+      });
+    Bookingdetail.findByIdAndUpdate(bookingid, req.body, function (err) {
+      if (err)
+        return processErrors(err, "bookingadd", req, res, { add: false });
+      res.redirect("/booking/bookings/" + userid);
     });
+  });
 });
 
 /* Process the product return, sent as GET request, for the given product Id. */
 router.get("/cancel/:bookingid", function (req, res, next) {
   const bookingId = req.params.bookingid;
-  // const custId = 0;
-  // Booking.findOne({ _id: bookingId }, (err, oneBooking) => {
-  //   if (err) console.log(err);
-  //   console.log(`******* This is the returned booking to DELETE ${oneBooking} ******`)
-  //   custId = oneBooking.CustomerId;
-  // });
+  Bookingdetail.findOneAndDelete({ BookingId: bookingId }, (err) => {
+    if (err) console.log(err);
+    next;
+  });
   Booking.findOneAndDelete({ _id: bookingId }, (err) => {
     if (err) console.log(err);
     res.redirect("/"); // Redirect to the purchases page
